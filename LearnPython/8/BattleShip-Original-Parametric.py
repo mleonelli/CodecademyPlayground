cc395a415d21b278588af71503ea1a80d02df1f1
Mauro Leonelli <mleonelli@gmail.com>
2018-04-10 18:22:03 +0200

    Create BattleShip-Original-Parametric.py

---
+++ LearnPython/8/BattleShip-Original-Parametric.py
@@ -0,0 +1,55 @@
+from random import randint
+
+#The board is a square of n size
+n = 10
+
+#The game is in t turns
+t = 5
+
+board = []
+
+for x in range(n):
+  board.append(["O"] * n)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+for turn in range(t):
+  print "Turn", turn + 1
+  guess_row = int(raw_input("Guess Row: "))
+  guess_col = int(raw_input("Guess Col: "))
+
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sunk my battleship!"
+    break
+  else:
+    if (guess_row < 0 or guess_row > n - 1) or \
+    (guess_col < 0 or guess_col > n - 1):
+      print "Oops, that's not even in the ocean."
+    elif(board[guess_row][guess_col] == "X"):
+      print "You guessed that one already."
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+    # Print (turn + 1) here!
+    print_board(board)
+    
+  if turn == t - 1:
+    print "Game Over"
+  	
